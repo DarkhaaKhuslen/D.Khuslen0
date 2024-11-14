6a5d10e190b259ab911191e0baedf5836a80af47
DarkhaaKhuslen <167779315+DarkhaaKhuslen@users.noreply.github.com>
2024-11-14 14:33:10 +0800

    D.Khuslen0

---
+++ app.js
@@ -0,0 +1,42 @@
+// console.log -> hewleh func
+console.log("hello world");
+console.log(5*8/100+666);
+console.log("5+5="+(5+5));
+// variable -> huwisagch
+var a=5;
+console.log(a+10);
+console.log("a");
+var name = "Khuslen", age=16;
+console.log("My name is "+name+ " . i am "+age+"years old.");
+var too1=5, too2=5;
+console.log(too1+too2);
+var urt=4, orgon=2;
+var p,s;
+console.log("Tegsh " + " ontsogttiin " + " urt n " + urt);
+console.log("Orgon " + orgon);
+console.log("p=(4+2)*2="+(2*6));
+console.log("s=4*2="+ (4*2));
+console.log("p:"+((urt+orgon)*2));
+console.log("s:"+(urt*orgon));
+// prompt() -> garaas utga oruulah 
+//var ner = prompt("Khuslen");
+//alert("Tanii oruulsan ner "+ ner);
+// var nas = +prompt("Nasaa oruulna uu");
+//alert("Tanii torson on : "+(2024-17));
+// NaN -> not a number
+// math.floor(2.754);-> 2
+// 42 -> 4+2=6
+// 42 -> 42/10 -> math.floor(4,2) -> 4
+// 42 -> 42%10 -> 2
+//var too=+prompt("2 orontoi too oruulna uu");
+//var neg= Math.floor(too/10) ; //Math.floor(58/10)->4
+//console.log("ehnii oron :"+neg);
+//var hoyr=too%10;
+//alert(neg+hoyr);
+var too1=+prompt("3 orontoi too oruulna uu"); // 134
+var neg1=Math.floor(too1/100); // 1
+console.log("ehnii oron :"+ neg1);
+var hoyr=too1%100; // 134%100 
+alert((neg1+hoyr)%10);
+
+
